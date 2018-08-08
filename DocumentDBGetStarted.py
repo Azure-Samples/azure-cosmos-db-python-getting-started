fb4cf6244172b8249a4cdc47859b315a1f0c5731
Alex Waters <22768512+alex-waters@users.noreply.github.com>
2018-08-08 21:06:14 +0100

    tidyup a few lines
    quick tidy up to remove redundant import and semi-colons.
    also shortened lines to adhere to PEP8

--- DocumentDBGetStarted.py
+++ DocumentDBGetStarted.py
@@ -1,4 +1,3 @@
-import pydocumentdb;
 import pydocumentdb.document_client as document_client
 
 config = { 
@@ -6,10 +5,13 @@ config = {
     'MASTERKEY': 'FILLME',
     'DOCUMENTDB_DATABASE': 'db',
     'DOCUMENTDB_COLLECTION': 'coll'
-};
+}
 
 # Initialize the Python DocumentDB client
-client = document_client.DocumentClient(config['ENDPOINT'], {'masterKey': config['MASTERKEY']})
+client = document_client.DocumentClient(
+    config['ENDPOINT'],
+    {'masterKey': config['MASTERKEY']}
+)
 
 # Create a database
 db = client.CreateDatabase({ 'id': config['DOCUMENTDB_DATABASE'] })
@@ -22,7 +24,11 @@ options = {
 }
 
 # Create a collection
-collection = client.CreateCollection(db['_self'], { 'id': config['DOCUMENTDB_COLLECTION'] }, options)
+collection = client.CreateCollection(
+    db['_self'],
+    { 'id': config['DOCUMENTDB_COLLECTION'] },
+    options
+)
 
 # Create some documents
 document1 = client.CreateDocument(collection['_self'],
@@ -32,7 +38,8 @@ document1 = client.CreateDocument(collection['_self'],
         'Cloud Service': 0,
         'Virtual Machine': 0,
         'name': 'some' 
-    })
+    }
+)
 
 document2 = client.CreateDocument(collection['_self'],
     { 
@@ -41,16 +48,17 @@ document2 = client.CreateDocument(collection['_self'],
         'Cloud Service': 0,
         'Virtual Machine': 0,
         'name': 'some' 
-    })
+    }
+)
 
 # Query them in SQL
-query = { 'query': 'SELECT * FROM server s' }    
+query = { 'query': 'SELECT * FROM server' }    
         
 options = {} 
 options['enableCrossPartitionQuery'] = True
 options['maxItemCount'] = 2
 
 result_iterable = client.QueryDocuments(collection['_self'], query, options)
-results = list(result_iterable);
+results = list(result_iterable)
 
 print(results)
