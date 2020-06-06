109d6eb971635680bf8a27be804dfc8cc741a597
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2020-06-05 22:27:04 -0400

    * Adding Reconnection to show how to connect to existing database/container * changing out <> to region which is supported by Pycharm and VSCode (two most popular Python development environments) * Adding documentation about partition keys * Trying to respect PEP 8 better

--- cosmos_get_started.py
+++ cosmos_get_started.py
@@ -5,47 +5,62 @@ import family
 endpoint = "endpoint"
 key = 'primary_key'
 
-# <create_cosmos_client>
+#region Create Cosmos Client
 client = CosmosClient(endpoint, key)
-# </create_cosmos_client>
+#endregion
 
 # Create a database
-# <create_database_if_not_exists>
+#region Create a Database
 database_name = 'AzureSampleFamilyDatabase'
 database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
+#endregion
 
 # Create a container
 # Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
+# See detailed documentation on partition keys here:
+# https://docs.microsoft.com/en-us/azure/cosmos-db/partitioning-overview
+
+#region Create Container if doesn't exist
 container_name = 'FamilyContainer'
 container = database.create_container_if_not_exists(
     id=container_name, 
     partition_key=PartitionKey(path="/lastName"),
     offer_throughput=400
 )
-# </create_container_if_not_exists>
+#endregion
 
+# Reconnecting to Azure Cosmos DB after creating database and container
+# This isn't normal but included to give example of how to connect to existing database and container
+# without creating a new database and container
+#region Reconnection
+client = CosmosClient(endpoint, key)
+database = client.get_database_client(database=database_name)
+container = database.get_container_client(container=container_name)
+#endregion
 
 # Add items to the container
-family_items_to_create = [family.get_andersen_family_item(), family.get_johnson_family_item(), family.get_smith_family_item(), family.get_wakefield_family_item()]
+family_items_to_create = [family.get_andersen_family_item(), 
+    family.get_johnson_family_item(), 
+    family.get_smith_family_item(), 
+    family.get_wakefield_family_item()]
 
- # <create_item>
+#region Create Items
 for family_item in family_items_to_create:
     container.create_item(body=family_item)
-# </create_item>
+#endregion
 
 # Read items (key value lookups by partition key and id, aka point reads)
-# <read_item>
+#region Read Item
 for family in family_items_to_create:
     item_response = container.read_item(item=family['id'], partition_key=family['lastName'])
     request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
     print('Read item with id {0}. Operation consumed {1} request units'.format(item_response['id'], (request_charge)))
-# </read_item>
+#endregion
 
 # Query these items using the SQL query syntax. 
-# Specifying the partition key value in the query allows Cosmos DB to retrieve data only from the relevant partitions, which improves performance
-# <query_items>
+# Specifying the partition key value in the query allows Cosmos DB 
+# to retrieve data only from the relevant partitions, which improves performance
+#region Query items by Partition key
 query = "SELECT * FROM c WHERE c.lastName IN ('Wakefield', 'Andersen')"
 
 items = list(container.query_items(
@@ -56,4 +71,4 @@ items = list(container.query_items(
 request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
 
 print('Query returned {0} items. Operation consumed {1} request units'.format(len(items), request_charge))
-# </query_items>
+#endregion
